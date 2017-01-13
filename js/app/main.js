32e30f5b3df94f7978e6d5e154f0b5a001637c3a
Samson Kapeyi <skapeyi@bitbucket.org>
2017-01-13 21:47:27 +0300

    Set up all the other controllers and changed the sidebar menu

--- js/app/main.js
+++ js/app/main.js
@@ -1,20 +1,50 @@
 var app = angular.module('app', [
-    'ngRoute', 
+    'ngRoute',
 
 ]);
 app.config(['$routeProvider', function ($routeProvider) {
-   $routeProvider.when("/", {
-       controller: "HomeController",
-        templateUrl: "js/app/partials/home.html"
-   });
+    $routeProvider.when("/alerts", {
+        controller: "AlertController",
+        templateUrl: "js/app/partials/alert.html"
+    });
 
-   $routeProvider.when("/district", {
-       controller: "DistrictController",
-       templateUrl: "js/app/partials/district.html"
-   });
-    $routeProvider.when("/facility", {
-       controller: "FacilityController",
-       templateUrl: "js/app/partials/facility.html"
-      });
+    $routeProvider.when("/expiry", {
+        controller: "ExpiryController",
+        templateUrl: "js/app/partials/expiry.html"
+    });
+    $routeProvider.when("/", {
+        controller: "HomeController",
+        templateUrl: "js/app/partials/home.html"
+    });
+
+    $routeProvider.when("/inventory", {
+        controller: "InventoryController",
+        templateUrl: "js/app/partials/inventory.html"
+    });
+
+    $routeProvider.when("/stocklookup", {
+        controller: "LookupController",
+        templateUrl: "js/app/partials/lookup.html"
+    });
+
+    $routeProvider.when("/orders", {
+        controller: "OrdersController",
+        templateUrl: "js/app/partials/order.html"
+    });
+
+    $routeProvider.when("/returns", {
+        controller: "ReturnController",
+        templateUrl: "js/app/partials/returns.html"
+    });
+
+    $routeProvider.when("/stock", {
+        controller: "StockController",
+        templateUrl: "js/app/partials/stock.html"
+    });
+
+    $routeProvider.when("/supplies", {
+        controller: "SuppliesController",
+        templateUrl: "js/app/partials/supplies.html"
+    });
 
 }]);
